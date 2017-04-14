ea275ac50d2697748dcb20c556d31a82efec8337
v21k <vlad.kornieiev@gmail.com>
2017-04-14 14:24:44 +0300

    changed signature - String ---> List<String>

--- src/main/java/ua/artcode/utils/StatsUtils.java
+++ src/main/java/ua/artcode/utils/StatsUtils.java
@@ -1,8 +1,9 @@
 package ua.artcode.utils;
 
-import org.apache.commons.lang3.StringUtils;
 import ua.artcode.model.CheckResult;
 import ua.artcode.model.GeneralResponse;
+
+import java.util.List;
 
 /**
  * Created by v21k on 13.04.17.
@@ -11,24 +12,30 @@ public class StatsUtils {
     /**
      * Parsing info from input string with results
      *
-     * @param testsInfo - information abouts tests in form:
+     * @param testsInfo - information abouts tests in form List(String), where each String like:
      *                  Result: true/false, expected: ..., actual: ...
      * @return CheckResult model with parsed information
      * @see CheckResult
      */
-    public static CheckResult stats(String testsInfo) {
-        int overallTests = StringUtils.countMatches(testsInfo, "Result");
-        int failedTests = StringUtils.countMatches(testsInfo, "false");
+    public static CheckResult stats(List<String> testsInfo) {
+        int overallTests = testsInfo.size();
+        int failedTests = 0;
+
+        for (String test : testsInfo) {
+            failedTests = test.contains("false") ? failedTests + 1 : failedTests;
+        }
+
         int passedTests = overallTests - failedTests;
 
-        String testsStats = String.format("Tests overall: %d, passed: %d, failed: %d\n%s",
+        String testsStats = String.format("Tests overall: %d, passed: %d, failed: %d\n",
                 overallTests,
                 passedTests,
-                failedTests,
-                failedTests == 0 ? new GeneralResponse("DONE") : new GeneralResponse("FAILED"));
+                failedTests);
+
+        GeneralResponse result = failedTests == 0 ? new GeneralResponse("PASSED") : new GeneralResponse("FAILED");
 
 
-        return new CheckResult(overallTests, passedTests, failedTests, testsInfo, testsStats);
+        return new CheckResult(overallTests, passedTests, failedTests, testsInfo, testsStats, result);
     }
 
 }
