29ffb97f0906f062165cdf0a81fb1ae0b7ae9f45
v21k <vlad.kornieiev@gmail.com>
2017-04-14 15:26:18 +0300

    added @PropertySoruce

--- src/main/java/ua/artcode/controller/Application.java
+++ src/main/java/ua/artcode/controller/Application.java
@@ -2,8 +2,12 @@ package ua.artcode.controller;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 
 @SpringBootApplication(scanBasePackages = {"ua.artcode"})
+@PropertySource("classpath:app.properties")
 public class Application {
 
     public static void main(String[] args) {
