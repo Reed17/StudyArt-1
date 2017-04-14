b9b875001d50fec860f5f3c21625b7d2cbafbb92
v21k <vlad.kornieiev@gmail.com>
2017-04-14 12:20:08 +0300

    extraced method addSolution() to StringUtils

--- src/main/java/ua/artcode/utils/StringUtils.java
+++ src/main/java/ua/artcode/utils/StringUtils.java
@@ -1,7 +1,13 @@
 package ua.artcode.utils;
 
+import ua.artcode.model.SolutionModel;
+
 /**
  * Created by v21k on 14.04.17.
  */
 public class StringUtils {
+    public static String addSoulution(String source, SolutionModel solution) {
+        return source.substring(0, source.lastIndexOf("}"))
+                + solution.getSolution() + "}";
+    }
 }
