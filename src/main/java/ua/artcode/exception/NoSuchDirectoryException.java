ce99eadbf401faadc7b75f162a3de533d00a2477
v21k <vlad.kornieiev@gmail.com>
2017-04-14 11:36:10 +0300

    refactored

---
+++ src/main/java/ua/artcode/exception/NoSuchDirectoryException.java
@@ -0,0 +1,10 @@
+package ua.artcode.exception;
+
+/**
+ * Created by v21k on 14.04.17.
+ */
+public class NoSuchDirectoryException extends AppException {
+    public NoSuchDirectoryException(String message) {
+        super(message);
+    }
+}
