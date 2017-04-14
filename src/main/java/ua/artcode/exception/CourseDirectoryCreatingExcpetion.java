51b8ca6bfb23dbad6c0bf28abcd53c022c0e0c8e
v21k <vlad.kornieiev@gmail.com>
2017-04-14 13:12:13 +0300

    CourseDirectoryCreatingException added

---
+++ src/main/java/ua/artcode/exception/CourseDirectoryCreatingExcpetion.java
@@ -0,0 +1,10 @@
+package ua.artcode.exception;
+
+/**
+ * Created by v21k on 14.04.17.
+ */
+public class CourseDirectoryCreatingExcpetion extends AppException {
+    public CourseDirectoryCreatingExcpetion(String message) {
+        super(message);
+    }
+}
