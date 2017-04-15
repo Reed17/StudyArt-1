a46983fd6941fa60edc49eb10fa470c02c3c3ae0
v21k <vlad.kornieiev@gmail.com>
2017-04-15 18:02:25 +0300

    core to run .class files

--- src/main/java/ua/artcode/core/RunCore.java
+++ src/main/java/ua/artcode/core/RunCore.java
@@ -1,7 +1,44 @@
 package ua.artcode.core;
 
+import ua.artcode.utils.StringUtils;
+
+import javax.tools.JavaCompiler;
+import javax.tools.ToolProvider;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLClassLoader;
+
 /**
  * Created by v21k on 15.04.17.
  */
 public class RunCore {
+    private static final JavaCompiler COMPILER = ToolProvider.getSystemJavaCompiler();
+
+    public static String runClassWithMain(String classPath) throws MalformedURLException, ClassNotFoundException, NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+        ByteArrayOutputStream baosOut = new ByteArrayOutputStream();
+        ByteArrayOutputStream baosErr = new ByteArrayOutputStream();
+
+        COMPILER.run(null, baosOut, baosErr, classPath);
+
+        String errors = baosErr.toString();
+        String out = baosOut.toString();
+
+        String className = StringUtils.getClassNameFromClassPath(classPath);
+        String classRoot = StringUtils.getClassRootFromClassPath(classPath);
+
+        URLClassLoader classLoader = URLClassLoader.newInstance(new URL[]{new File(classRoot).toURI().toURL()});
+        Class<?> cls = Class.forName(className, true, classLoader);
+
+        Method method = cls.getMethod("main", String[].class);
+        String[] args = null;
+        method.invoke(null, (Object) args);
+
+        return errors.length() > 0 ? errors : out;
+    }
+
+
 }
