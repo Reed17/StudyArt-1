c336a2c645ef71fb49c15ecd541caa722e53ab96
v21k <vlad.kornieiev@gmail.com>
2017-04-15 12:24:53 +0300

    added field id

---
+++ src/main/java/ua/artcode/dao/CourseDB.java
@@ -0,0 +1,7 @@
+package ua.artcode.dao;
+
+/**
+ * Created by v21k on 15.04.17.
+ */
+public class CourseDB {
+}
--- src/main/java/ua/artcode/model/Course.java
+++ src/main/java/ua/artcode/model/Course.java
@@ -6,6 +6,7 @@ import java.util.List;
  * Created by v21k on 15.04.17.
  */
 public class Course {
+    private int id;
     private String name;
     private String author;
     private String url;
@@ -15,12 +16,27 @@ public class Course {
     public Course() {
     }
 
-    public Course(String name, String author, String url, String localPath, List<Lesson> lessons) {
+    public Course(int id, String name, String author, String url, String localPath, List<Lesson> lessons) {
+        this.id = id;
         this.name = name;
         this.author = author;
         this.url = url;
         this.localPath = localPath;
         this.lessons = lessons;
+    }
+
+    public Course(String name, String author, String url) {
+        this.name = name;
+        this.author = author;
+        this.url = url;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
     }
 
     public String getName() {
@@ -70,27 +86,26 @@ public class Course {
 
         Course course = (Course) o;
 
+        if (id != course.id) return false;
         if (name != null ? !name.equals(course.name) : course.name != null) return false;
         if (author != null ? !author.equals(course.author) : course.author != null) return false;
-        if (url != null ? !url.equals(course.url) : course.url != null) return false;
-        if (localPath != null ? !localPath.equals(course.localPath) : course.localPath != null) return false;
-        return lessons != null ? lessons.equals(course.lessons) : course.lessons == null;
+        return url != null ? url.equals(course.url) : course.url == null;
     }
 
     @Override
     public int hashCode() {
-        int result = name != null ? name.hashCode() : 0;
+        int result = id;
+        result = 31 * result + (name != null ? name.hashCode() : 0);
         result = 31 * result + (author != null ? author.hashCode() : 0);
         result = 31 * result + (url != null ? url.hashCode() : 0);
-        result = 31 * result + (localPath != null ? localPath.hashCode() : 0);
-        result = 31 * result + (lessons != null ? lessons.hashCode() : 0);
         return result;
     }
 
     @Override
     public String toString() {
         return "Course{" +
-                "name='" + name + '\'' +
+                "id=" + id +
+                ", name='" + name + '\'' +
                 ", author='" + author + '\'' +
                 ", url='" + url + '\'' +
                 ", localPath='" + localPath + '\'' +
@@ -98,3 +113,4 @@ public class Course {
                 '}';
     }
 }
+
