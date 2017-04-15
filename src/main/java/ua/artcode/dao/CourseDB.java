4b690d09c7f97163653bbdb83803e21c6a31a59e
v21k <vlad.kornieiev@gmail.com>
2017-04-15 13:41:14 +0300

    add() and getbyid() added

--- src/main/java/ua/artcode/dao/CourseDB.java
+++ src/main/java/ua/artcode/dao/CourseDB.java
@@ -1,7 +1,61 @@
 package ua.artcode.dao;
 
+import org.eclipse.jgit.api.errors.GitAPIException;
+import org.springframework.stereotype.Component;
+import ua.artcode.exceptions.DirectoryCreatingException;
+import ua.artcode.exceptions.InvalidIDException;
+import ua.artcode.exceptions.LessonsParsingException;
+import ua.artcode.model.Course;
+import ua.artcode.utils.IOUtils;
+
+import java.io.IOException;
+import java.util.Collection;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
 /**
  * Created by v21k on 15.04.17.
  */
-public class CourseDB {
+@Component
+public class CourseDB implements StudyDB<Course> {
+    private Map<Integer, Course> courseMap;
+
+    public CourseDB() {
+        this.courseMap = new ConcurrentHashMap<>();
+    }
+
+    @Override
+    public boolean add(Course course) throws GitAPIException, DirectoryCreatingException, LessonsParsingException {
+        course.setId(courseMap.size() + 1);
+        course.setLocalPath(IOUtils.saveLocally(course));
+        try {
+            course.setLessons(IOUtils.getLessons(course));
+        } catch (IOException e) {
+            throw new LessonsParsingException("Can't parse lessons for course: " + course.getName());
+        }
+
+        return !courseMap.values().contains(course) && courseMap.put(course.getId(), course) == null;
+    }
+
+    @Override
+    public boolean remove(int id) {
+        return false;
+    }
+
+    @Override
+    public Collection<Course> getAll() {
+        return null;
+    }
+
+    @Override
+    public Course getByID(int id) throws InvalidIDException {
+        if (id <= 0) {
+            throw new InvalidIDException("ID must be > 0");
+        }
+        return courseMap.values()
+                .stream()
+                .filter(course -> course.getId() == id)
+                .findFirst()
+                .orElse(null);
+    }
 }
--- src/main/java/ua/artcode/dao/StudyDB.java
+++ src/main/java/ua/artcode/dao/StudyDB.java
@@ -1,7 +1,18 @@
 package ua.artcode.dao;
 
+import org.eclipse.jgit.api.errors.GitAPIException;
+import ua.artcode.exceptions.DirectoryCreatingException;
+import ua.artcode.exceptions.InvalidIDException;
+import ua.artcode.exceptions.LessonsParsingException;
+
+import java.util.Collection;
+
 /**
  * Created by v21k on 15.04.17.
  */
-public interface StudyDB {
+public interface StudyDB<T> {
+    boolean add(T T) throws GitAPIException, DirectoryCreatingException, LessonsParsingException;
+    boolean remove(int id);
+    Collection<T> getAll();
+    T getByID(int id) throws InvalidIDException;
 }
