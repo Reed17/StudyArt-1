6f10ed591a65ccf9bc0795cf7d4975f0c7ff9135
v21k <vlad.kornieiev@gmail.com>
2017-04-15 18:01:26 +0300

    run service to run classes, metods etc

--- src/main/java/ua/artcode/service/RunService.java
+++ src/main/java/ua/artcode/service/RunService.java
@@ -1,7 +1,15 @@
 package ua.artcode.service;
 
+import ua.artcode.model.ExternalCode;
+import ua.artcode.model.RunResults;
+
+import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+import java.net.MalformedURLException;
+
 /**
  * Created by v21k on 15.04.17.
  */
-public class RunService {
+public interface RunService {
+    RunResults runClass(ExternalCode code) throws ClassNotFoundException, IOException, InvocationTargetException, IllegalAccessException, NoSuchMethodException;
 }
--- src/main/java/ua/artcode/service/RunServiceImpl.java
+++ src/main/java/ua/artcode/service/RunServiceImpl.java
@@ -1,7 +1,24 @@
 package ua.artcode.service;
 
+import org.springframework.stereotype.Service;
+import ua.artcode.core.RunCore;
+import ua.artcode.model.ExternalCode;
+import ua.artcode.model.RunResults;
+import ua.artcode.utils.IOUtils;
+
+import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+
 /**
  * Created by v21k on 15.04.17.
  */
-public class RunServiceImpl {
+@Service
+public class RunServiceImpl implements RunService {
+
+
+    @Override
+    public RunResults runClass(ExternalCode code) throws ClassNotFoundException, IOException, InvocationTargetException, IllegalAccessException, NoSuchMethodException {
+        String path = IOUtils.saveExternalCodeLocally(code.getSourceCode());
+        return new RunResults(RunCore.runClassWithMain(path));
+    }
 }
