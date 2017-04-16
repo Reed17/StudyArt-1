e3d2c14d01aa4a74920a98efbabca8da96d1bba2
v21k <vlad.kornieiev@gmail.com>
2017-04-16 20:54:44 +0300

    added functional interface to process results

---
+++ src/main/java/ua/artcode/core/results_processor/MethodRusultsProcessor.java
@@ -0,0 +1,21 @@
+package ua.artcode.core.results_processor;
+
+import ua.artcode.core.method_runner.MethodRunner;
+import ua.artcode.model.RunResults;
+
+/**
+ * Created by v21k on 16.04.17.
+ */
+
+@FunctionalInterface
+public interface MethodRusultsProcessor {
+    /**
+     * Processing results from MethodRunner(string)
+     *
+     * @param runner MethodRunner with logic for running certain method
+     * @param args   - String args in next order: compilation errors, systemOut, methodResult
+     * @return RunResult model with all info
+     * @see MethodRunner
+     */
+    RunResults process(MethodRunner runner, String... args);
+}
---
+++ src/main/java/ua/artcode/core/results_processor/ResultsProcessors.java
@@ -0,0 +1,13 @@
+package ua.artcode.core.results_processor;
+
+import ua.artcode.model.RunResults;
+
+public class ResultsProcessors {
+    public static int ERORR = 0;
+    public static int SYSTEM_OUT = 1;
+    public static int METHOD_RESULTS = 2;
+
+    public static MethodRusultsProcessor mainProcessor = ((runner, args) -> {
+        return args[ERORR].length() > 0 ? new RunResults(args[ERORR]) : new RunResults(args[SYSTEM_OUT]);
+    });
+}
