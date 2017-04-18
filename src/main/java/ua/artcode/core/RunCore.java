cd9f489cabf52ee1402c857a79519c114b0f512d
v21k <vlad.kornieiev@gmail.com>
2017-04-18 10:34:35 +0300

    pre processor added and other minor fixes

--- src/main/java/ua/artcode/core/RunCore.java
+++ src/main/java/ua/artcode/core/RunCore.java
@@ -2,11 +2,11 @@ package ua.artcode.core;
 
 import ua.artcode.core.method_checkers.MethodChecker;
 import ua.artcode.core.method_runner.MethodRunner;
-import ua.artcode.core.results_processor.MethodRusultsProcessor;
+import ua.artcode.core.post_processor.MethodResultsProcessor;
+import ua.artcode.core.pre_processor.MethodRunnerPreProcessor;
 import ua.artcode.model.RunResults;
 import ua.artcode.utils.IOUtils;
 import ua.artcode.utils.RunUtils;
-import ua.artcode.utils.StringUtils;
 
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
@@ -17,20 +17,31 @@ import java.net.MalformedURLException;
  * Created by v21k on 15.04.17.
  */
 public class RunCore {
+    private static final int MAIN_CLASS_PATH = 0;
+    private static final int MAIN_CLASS_ROOT_PACKAGE = 1;
 
-    public static RunResults runMethod(String classPath, MethodChecker checker, MethodRunner runner, MethodRusultsProcessor processor) throws MalformedURLException, ClassNotFoundException, NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+    public static RunResults runMethod(String[] classPaths,
+                                       MethodRunnerPreProcessor preProcessor,
+                                       MethodChecker checker,
+                                       MethodRunner runner,
+                                       MethodResultsProcessor processor)
+            throws MalformedURLException,
+            ClassNotFoundException,
+            NoSuchMethodException,
+            InvocationTargetException,
+            IllegalAccessException {
+
         // compile and save results (errors)
-        String compilationErrors = RunUtils.compile(classPath);
+        String compilationErrors = RunUtils.compile(classPaths);
 
-        // parse class name and root package for this class
-        String className = StringUtils.getClassNameFromClassPath(classPath);
-        String classRoot = StringUtils.getClassRootFromClassPath(classPath);
+        // prepare array with main class path and it's root package
+        String[] paths = preProcessor.getPaths(classPaths);
 
-        // getting class
-        Class<?> cls = RunUtils.getClass(className, classRoot);
+        // getting a class
+        Class<?> cls = RunUtils.getClass(paths[MAIN_CLASS_PATH], paths[MAIN_CLASS_ROOT_PACKAGE]);
 
         // checking method(s) needed
-        if(!checker.checkMethods(cls)){
+        if (!checker.checkMethods(cls)) {
             return new RunResults("Can't find required method(s)");
         }
 
@@ -47,7 +58,6 @@ public class RunCore {
         // return RunResult
         return processor.process(runner, compilationErrors, systemOut, methodOutput);
     }
-
 
 
 }
