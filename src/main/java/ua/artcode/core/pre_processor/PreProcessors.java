ebf4e6e6e699e474defeea58ec7839acaf75df7c
v21k <vlad.kornieiev@gmail.com>
2017-04-18 10:34:13 +0300

    pre-processor func interface added

---
+++ src/main/java/ua/artcode/core/pre_processor/PreProcessors.java
@@ -0,0 +1,22 @@
+package ua.artcode.core.pre_processor;
+
+import ua.artcode.utils.StringUtils;
+
+/**
+ * Created by v21k on 17.04.17.
+ */
+public class PreProcessors {
+    public static MethodRunnerPreProcessor singleClass = (classPaths -> {
+        String classPath = classPaths[0];
+        return StringUtils.getClassNameAndRootFolder(classPath);
+    });
+
+    public static MethodRunnerPreProcessor lessons = (classPaths -> {
+        String classPath = StringUtils.getMainClassPath(classPaths);
+
+        // todo extract to StringUtils
+        String className = classPath.substring(classPath.lastIndexOf("src/")+4, classPath.lastIndexOf(".")).replace("/", ".");
+        String rootPath = classPath.substring(0, classPath.lastIndexOf("src/")) + "src/";
+        return new String[] {className, rootPath};
+    });
+}
