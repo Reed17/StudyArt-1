87a5d82d311cffd08420421b7d04b1468deb74a7
v21k <vlad.kornieiev@gmail.com>
2017-04-19 10:20:17 +0300

    replaced with stream

--- src/main/java/ua/artcode/core/method_checkers/Checkers.java
+++ src/main/java/ua/artcode/core/method_checkers/Checkers.java
@@ -1,19 +1,12 @@
 package ua.artcode.core.method_checkers;
 
 import java.lang.reflect.Method;
+import java.util.Arrays;
 
 /**
  * Created by v21k on 16.04.17.
  */
 public class Checkers {
-    public static MethodChecker main = ((cls) -> {
-        Method[] methods = cls.getDeclaredMethods();
-        // todo use streams
-        for (Method method : methods) {
-            if (method.getName().equals("main")) {
-                return true;
-            }
-        }
-        return false;
-    });
+    public static MethodChecker main = ((cls) -> Arrays.stream(cls.getDeclaredMethods())
+            .anyMatch(method -> method.getName().equals("main")));
 }
