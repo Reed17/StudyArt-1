c2cbb390f86afc44241506c6f80036b097be702a
v21k <vlad.kornieiev@gmail.com>
2017-04-24 13:22:28 +0300

    few changes corresponding new logic in other core parts

--- src/main/java/ua/artcode/core/RunCore.java
+++ src/main/java/ua/artcode/core/RunCore.java
@@ -4,10 +4,12 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import ua.artcode.core.checker.ClassChecker;
 import ua.artcode.core.method_runner.MethodRunner;
 import ua.artcode.core.post_processor.MethodResultsProcessor;
 import ua.artcode.core.pre_processor.MethodRunnerPreProcessor;
 import ua.artcode.model.response.GeneralResponse;
+import ua.artcode.model.response.MethodStats;
 import ua.artcode.model.response.RunResults;
 import ua.artcode.utils.IO_utils.CommonIOUtils;
 import ua.artcode.utils.RunUtils;
@@ -23,10 +25,6 @@ import java.lang.reflect.InvocationTargetException;
  */
 @Component
 public class RunCore {
-    // constants with indexes for paths array (which contains Course className and root package).
-    private static final int MAIN_CLASS_PATH = 0;
-    private static final int MAIN_CLASS_ROOT_PACKAGE = 1;
-
     private static final Logger LOGGER = LoggerFactory.getLogger(RunCore.class);
 
     @Autowired
@@ -34,7 +32,7 @@ public class RunCore {
 
     public RunResults runMethod(String[] classPaths,
                                 MethodRunnerPreProcessor preProcessor,
-                                String methodName,
+                                ClassChecker checker,
                                 MethodRunner runner,
                                 MethodResultsProcessor postProcessor)
             throws IOException,
@@ -53,41 +51,27 @@ public class RunCore {
         LOGGER.info("Compilation - OK");
 
         // prepare array with classes
-        Class<?>[] classes = preProcessor.getClasses(classPaths, methodName);
+        Class<?>[] classes = preProcessor.getClasses(classPaths);
 
         // checking method(s) needed
-        if (!RunUtils.checkMethods(classes, methodName)) {
-            LOGGER.error("Method check - FAILED. Can't fine method {}", methodName);
+        if (!checker.checkClasses(classes)) {
+            LOGGER.error("Check classes for methods/annotations - FAILED");
             return new RunResults(new GeneralResponse("Can't find required method(s)"));
         }
-        LOGGER.info("Method check - OK");
+
+        LOGGER.info("Check classes for methods/annotations - OK");
 
         // call runner
-        String[] methodOutput = callRunner(runner, classes);
-
-        // return RunResult
-        return postProcessor.process(methodOutput);
-    }
-
-    /**
-     * @return String array with next info:
-     * 1. index 0 - runtime exceptions
-     * 2. index 1 - system.out
-     * 3. index 2 - methodOutput
-     */
-    private String[] callRunner(MethodRunner runner, Class<?>[] cls)
-            throws IOException, NoSuchMethodException, IllegalAccessException {
-
         String systemError = null;
         String systemOut = null;
-        String methodOutput = null;
+        MethodStats stats = null;
 
         try (ByteArrayOutputStream redirectedSystemOut = new ByteArrayOutputStream()) {
             PrintStream systemOutOld = ioUtils.redirectSystemOut(redirectedSystemOut);
 
             // call method
             try {
-                methodOutput = runner.runMethod(cls);
+                stats = runner.runMethod(classes);
                 systemOut = ioUtils.resetSystemOut(redirectedSystemOut, systemOutOld);
                 LOGGER.info("Method call - OK");
             } catch (InvocationTargetException e) {
@@ -97,9 +81,9 @@ public class RunCore {
                 ioUtils.resetSystemOut(redirectedSystemOut, systemOutOld);
                 LOGGER.error("Method call - FAILED. Caused by: {}", systemError);
             }
-
-
-            return new String[]{systemError, systemOut, methodOutput};
         }
+
+        // return RunResult
+        return postProcessor.process(stats, systemError, systemOut);
     }
 }
