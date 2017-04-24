f48b8d25d379ec5c966b44b6c356c504b5552d49
v21k <vlad.kornieiev@gmail.com>
2017-04-24 11:07:51 +0300

    changed signature

--- src/main/java/ua/artcode/core/pre_processor/MethodRunnerPreProcessor.java
+++ src/main/java/ua/artcode/core/pre_processor/MethodRunnerPreProcessor.java
@@ -4,6 +4,8 @@ package ua.artcode.core.pre_processor;
  * Created by v21k on 17.04.17.
  */
 
+import java.net.MalformedURLException;
+
 /**
  * This functional interface is using for getting paths for
  * class and it's root folders
@@ -12,5 +14,5 @@ public interface MethodRunnerPreProcessor {
     /**
      * @return String[] - 1st element - path for runnable class, 2nd - root folder for project
      */
-    String[] getPaths(String[] classPaths) throws ClassNotFoundException;
+    Class<?>[] getClasses(String[] classPaths, String methodName) throws ClassNotFoundException, MalformedURLException;
 }
