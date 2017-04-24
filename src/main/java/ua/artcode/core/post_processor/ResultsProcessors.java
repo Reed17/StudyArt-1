7857c555ff6a0c8ba8947c18300f8efe37c66136
v21k <vlad.kornieiev@gmail.com>
2017-04-24 13:17:17 +0300

    changed signature

--- src/main/java/ua/artcode/core/post_processor/ResultsProcessors.java
+++ src/main/java/ua/artcode/core/post_processor/ResultsProcessors.java
@@ -1,17 +1,13 @@
 package ua.artcode.core.post_processor;
 
+import ua.artcode.model.response.GeneralResponse;
 import ua.artcode.model.response.MethodResult;
-import ua.artcode.model.response.MethodStats;
 import ua.artcode.model.response.RunResults;
 
 public class ResultsProcessors {
-    private static final int RUNTIME_EXCEPTIONS = 0;
-    private static final int SYSTEM_OUT = 1;
-    private static final int METHOD_OUTPUT = 2;
-    public static MethodResultsProcessor main = ((methodOutput) ->
-            new RunResults(
-                    new MethodResult(methodOutput[SYSTEM_OUT],
-                            methodOutput[RUNTIME_EXCEPTIONS],
-                            methodOutput[METHOD_OUTPUT]),
-                    new MethodStats()));
+
+    public static MethodResultsProcessor main = (((stats, systemErr, systemOut) -> new RunResults(
+            new MethodResult(systemOut, systemErr),
+            stats,
+            new GeneralResponse(stats != null && stats.getFailedTests() > 0 ? "FAILED" : "OK"))));
 }
