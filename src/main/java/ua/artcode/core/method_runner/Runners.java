600c534400d778474a9d5a842215c8cd79cb5d66
v21k <vlad.kornieiev@gmail.com>
2017-04-24 11:11:49 +0300

    hardcoded versions - to run application - need to be refactored

--- src/main/java/ua/artcode/core/method_runner/Runners.java
+++ src/main/java/ua/artcode/core/method_runner/Runners.java
@@ -1,16 +1,22 @@
 package ua.artcode.core.method_runner;
 
 import java.lang.reflect.Method;
+import java.util.Arrays;
 
 /**
  * Created by v21k on 16.04.17.
  */
 public class Runners {
-    public static MethodRunner main = (cls -> {
-        Method method = cls.getMethod("main", String[].class);
-        String[] argumentsForMain = null;
-        // todo test this place with a negative test
-        method.invoke(null, (Object) argumentsForMain);
+    public static MethodRunner main = (classes -> {
+        for (Class<?> aClass : classes) {
+            Method method = aClass.getMethod("main", String[].class);
+            String[] argumentsForMain = null;
+            // todo test this place with a negative test
+            method.invoke(null, (Object) argumentsForMain);
+        }
         return "";
     });
+
+    // todo get JUnitCore.class and method run() and pass classes there
+
 }
--- src/main/java/ua/artcode/core/pre_processor/PreProcessors.java
+++ src/main/java/ua/artcode/core/pre_processor/PreProcessors.java
@@ -1,19 +1,45 @@
 package ua.artcode.core.pre_processor;
 
+import ua.artcode.utils.RunUtils;
 import ua.artcode.utils.StringUtils;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * Created by v21k on 17.04.17.
  */
 public class PreProcessors {
-    public static MethodRunnerPreProcessor singleClass = (classPaths -> {
-        String classPath = classPaths[0];
-        return StringUtils.getClassNameAndRootFolder(classPath, "/");
+    // todo mvn go offline
+    public static MethodRunnerPreProcessor singleClass = ((classPaths, methodName) -> {
+        String singleClassPath = classPaths[0];
+
+        String classPath = StringUtils.getClassNameFromClassPath(singleClassPath, "/");
+        String root = StringUtils.getClassRootFromClassPath(singleClassPath, "/");
+
+        return new Class[]{RunUtils.getClass(classPath, root)};
     });
 
-    public static MethodRunnerPreProcessor lessons = (classPaths -> {
-        String classPath = StringUtils.getClassPathByClassName(classPaths, "main");
-        return StringUtils.getClassNameAndRootFolder(classPath, "src/");
+    public static MethodRunnerPreProcessor lessons = ((classPaths, methodName) -> {
+        String root = StringUtils.getClassRootFromClassPath(classPaths[0], "src/");
+
+        int count = (int) Arrays.stream(classPaths)
+                .filter(path -> path.toLowerCase().contains(methodName))
+                .count();
+
+        Class<?>[] classes = new Class[count];
+
+        String[] classNames = Arrays.stream(classPaths)
+                .filter(path -> path.toLowerCase().contains(methodName))
+                .map(path -> StringUtils.getClassNameFromClassPath(path, "src/"))
+                .toArray(String[]::new);
+
+        for (int i = 0; i < classes.length; i++) {
+            classes[i] = RunUtils.getClass(classNames[i], root);
+        }
+
+        return classes;
     });
 
 
