ceb9aadeace6fb4d42b432ffcf5395e7fd41a72d
v21k <vlad.kornieiev@gmail.com>
2017-04-25 14:15:57 +0300

    Swagger UI added

--- src/main/java/ua/artcode/controller/CourseController.java
+++ src/main/java/ua/artcode/controller/CourseController.java
@@ -1,5 +1,6 @@
 package ua.artcode.controller;
 
+import io.swagger.annotations.ApiOperation;
 import org.eclipse.jgit.api.errors.GitAPIException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -28,6 +29,10 @@ public class CourseController {
     @Autowired
     private RunService runService;
 
+    @ApiOperation(httpMethod = "GET",
+            value = "Resource to get a course",
+            response = Course.class,
+            produces = "application/json")
     @RequestMapping(value = "/courses/get", method = RequestMethod.GET)
     public Course getCourseByID(@RequestParam int id) throws InvalidIDException, CourseNotFoundException {
         Course course = courseService.getByID(id);
@@ -35,6 +40,11 @@ public class CourseController {
         return course;
     }
 
+    @ApiOperation(httpMethod = "POST",
+            value = "Resource to add a course",
+            notes = "ID, lessons and localPath will be generated on server side, so you can pass 0/null or default values",
+            response = GeneralResponse.class,
+            produces = "application/json")
     @RequestMapping(value = "/courses/add", method = RequestMethod.POST)
     public GeneralResponse addCourse(@RequestBody @Valid Course course) {
         try {
@@ -53,6 +63,11 @@ public class CourseController {
         }
     }
 
+    @ApiOperation(httpMethod = "POST",
+            value = "Resource to run class (external code)",
+            notes = "Simple java class with main method",
+            response = RunResults.class,
+            produces = "application/json")
     @RequestMapping(value = "/run-class", method = RequestMethod.POST)
     public RunResults runClass(@RequestBody ExternalCode code) {
         try {
@@ -69,6 +84,11 @@ public class CourseController {
         }
     }
 
+    @ApiOperation(httpMethod = "GET",
+            value = "Resource to run class from lesson",
+            notes = "Runs a class in certain lesson (class must have main method)",
+            response = RunResults.class,
+            produces = "application/json")
     @RequestMapping(value = "/courses/lessons/run")
     public RunResults runLesson(@RequestParam int courseId,
                                 @RequestParam int lessonNumber) {
@@ -89,6 +109,11 @@ public class CourseController {
         }
     }
 
+    @ApiOperation(httpMethod = "POST",
+            value = "Resource to run class from lesson with solution (need to add solution before)",
+            notes = "Runs a class in certain lesson (class must have main method) which depends on Solution class",
+            response = RunResults.class,
+            produces = "application/json")
     @RequestMapping(value = "courses/lessons/send-solution-and-run", method = RequestMethod.POST)
     public RunResults runLessonWithSolution(@RequestParam int courseId,
                                             @RequestParam int lessonNumber,
