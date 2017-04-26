11841a3135dcb843462615a28a87d68d84018b83
v21k <vlad.kornieiev@gmail.com>
2017-04-26 12:18:39 +0300

    logging added

--- src/main/java/ua/artcode/core/method_checker/MethodCheckers.java
+++ src/main/java/ua/artcode/core/method_checker/MethodCheckers.java
@@ -1,6 +1,8 @@
 package ua.artcode.core.method_checker;
 
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.el.MethodNotFoundException;
 import java.util.Arrays;
@@ -9,11 +11,15 @@ import java.util.Arrays;
  * Created by v21k on 24.04.17.
  */
 public class MethodCheckers {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(MethodChecker.class);
+
     public static MethodChecker testChecker = (classes ->
     {
         if (!Arrays.stream(classes)
                 .allMatch(cls -> Arrays.stream(cls.getDeclaredMethods())
                         .anyMatch(method -> method.isAnnotationPresent(Test.class)))){
+            LOGGER.error("Check classes for methods/annotations - FAILED. @Test methods not found");
             throw new NoSuchMethodException("@Test methods not found");
         }
     });
@@ -22,6 +28,7 @@ public class MethodCheckers {
         if(!Arrays.stream(classes)
                 .allMatch(cls -> Arrays.stream(cls.getDeclaredMethods())
                         .anyMatch(method -> method.getName().equals("main")))){
+            LOGGER.error("Check classes for methods/annotations - FAILED. Main method not found");
             throw new NoSuchMethodException("Main method not found");
         }
     });
