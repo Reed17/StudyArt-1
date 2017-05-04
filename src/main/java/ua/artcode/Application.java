cb6895695ddf39cd10ddbe846edd6b820e3078e2
Vlad Kornieiev <vlad.kornieiev@gmail.com>
2017-05-04 18:21:39 +0300

    AspectJ enabled

--- src/main/java/ua/artcode/Application.java
+++ src/main/java/ua/artcode/Application.java
@@ -3,6 +3,7 @@ package ua.artcode;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
@@ -11,6 +12,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication(scanBasePackages = {"ua.artcode"})
 @EnableSwagger2 // http://localhost:8080/swagger-ui.html
+@EnableAspectJAutoProxy(proxyTargetClass = true)
 public class Application {
 
     public static void main(String[] args) {
