7f939f6df041b3b20d2c85c9cb3a94da63396612
ZheniaTrochun <zheniatrochun@ukr.net>
2017-05-20 01:44:25 +0300

    Security added, login added, tests fixed

--- src/main/java/ua/artcode/controller/ExceptionController.java
+++ src/main/java/ua/artcode/controller/ExceptionController.java
@@ -1,5 +1,7 @@
 package ua.artcode.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -10,9 +12,15 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 
 @ControllerAdvice
 public class ExceptionController extends ResponseEntityExceptionHandler {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(CourseController.class);
+
     @ExceptionHandler(value = { IllegalArgumentException.class, IllegalStateException.class })
     protected ResponseEntity<Object> handleConflict(RuntimeException ex, WebRequest request) {
         String bodyOfResponse = ex.getMessage();
+
+        LOGGER.error(bodyOfResponse);
+
         return handleExceptionInternal(ex, bodyOfResponse,
                 new HttpHeaders(), HttpStatus.BAD_REQUEST, request);
     }
