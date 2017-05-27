6355e2d997471221f2dcb9fdcca489189796000c
Vlad Kornieiev <vlad.kornieiev@gmail.com>
2017-05-27 17:45:10 +0300

    course repo update

--- server/src/main/java/ua/artcode/dao/repositories/CourseRepository.java
+++ server/src/main/java/ua/artcode/dao/repositories/CourseRepository.java
@@ -1,10 +1,7 @@
 package ua.artcode.dao.repositories;
 
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
 import ua.artcode.model.Course;
 
 @Repository
