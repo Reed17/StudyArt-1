8242cc6716d9cb251f677afff7579d79c950a93f
Vlad Kornieiev <vlad.kornieiev@gmail.com>
2017-05-27 17:44:56 +0300

    course repo update

--- server/src/main/java/ua/artcode/dao/repositories/CourseRepository.java
+++ server/src/main/java/ua/artcode/dao/repositories/CourseRepository.java
@@ -9,19 +9,5 @@ import ua.artcode.model.Course;
 
 @Repository
 public interface CourseRepository extends CrudRepository<Course, Integer> {
-
     Course findByNameAndAuthor(String name, String author);
-
-
-    @Transactional
-    @Modifying
-    @Query("update Course c set c.sourcesRoot  = ?1, c.testsRoot = ?2 where c.id = ?3")
-    int updateSourcesAndTestsRoot(String sourcesRoot, String testsRoot, int courseId);
-
-    @Transactional
-    @Modifying
-    @Query("update Course c set c.dependencies = ?1 where c.id = ?2")
-    int updateDependencies(String[] dependencies, int courseId);
-
-
 }
