db6e93ac9b31f08e8e0f373f381ae92c5525c602
alexzolkin19 <alexzolkin19@gmail.com>
2017-05-28 11:31:17 +0300

    Login fixes, it works now)

--- server/src/main/java/ua/artcode/model/dto/LoginRequestDTO.java
+++ server/src/main/java/ua/artcode/model/dto/LoginRequestDTO.java
@@ -15,5 +15,19 @@ public class LoginRequestDTO {
     public LoginRequestDTO() {
     }
 
+    public String getLogin() {
+        return login;
+    }
 
+    public String getPassword() {
+        return password;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
