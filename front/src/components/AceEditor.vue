6bb7ddf43b0a7e1d8b084c3449fe7f536221bcaf
Maks M <makspman@gmail.com>
2017-05-28 11:43:58 +0300

    runCode

---
+++ front/src/components/AceEditor.vue
@@ -0,0 +1,58 @@
+<template>
+  <div>
+    <brace style="height: 500px"
+           :fontsize="'12px'"
+           :theme="'github'"
+           :mode="'json'"
+           :codefolding="'markbegin'"
+           :softwrap="'free'"
+           :selectionstyle="'text'"
+           :highlightline="true"
+           @code-change="getCode($event)">
+    </brace>
+    <div>
+      <v-btn dark default @click.native="greet">Run code</v-btn>
+
+      <v-text-field
+        name="input-7-1"
+        label="Result"
+        :value="response"
+        @click="runCode()"
+        multi-line
+        disabled>
+      </v-text-field>
+    </div>
+
+  </div>
+</template>
+
+<script>
+  import Brace from 'vue-bulma-brace'
+
+  export default {
+    data(){
+      return {
+        response: '',
+        input: '',
+      }
+    },
+    components: {
+      Brace
+    },
+
+    methods: {
+      getCode(target){
+        this.$emit('code-change', target);
+        this.input = target;
+      },
+
+      runCode() {
+        axios.post('http://localhost:8080/courses/run-code', this.input)
+          .then((response) => {
+            this.response = response.data;
+          });
+      },
+    }
+
+  }
+</script>
--- front/src/router/index.js
+++ front/src/router/index.js
@@ -4,6 +4,7 @@ import Login from "@/components/Login";
 import Register from "@/components/Register";
 import Courses from "@/components/Courses";
 import Course from "@/components/Course";
+import AceEditor from "@/components/AceEditor";
 
 
 Vue.use(Router);
@@ -29,6 +30,11 @@ export default new Router({
       path: '/register',
       name: 'Register',
       component: Register
+    },
+    {
+      path: '/editor',
+      name: 'AceEditor',
+      component: AceEditor
     },
   ],
   mode: 'history'
