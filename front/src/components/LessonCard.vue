f6bc50b0a0d3f5d8f6b9c83d701f8bb4acc48c7f
Maks M <makspman@gmail.com>
2017-07-05 17:51:08 +0300

    lesson

--- front/src/components/LessonCard.vue
+++ front/src/components/LessonCard.vue
@@ -1,18 +1,53 @@
 <template>
-  <v-card>
+
+  <div>
+  <v-card-flat>
     <v-card-title>
       {{lesson.name}}
     </v-card-title>
     <v-card-text>
       <div v-html="compiledMarkdown"></div>
     </v-card-text>
-  </v-card>
+    <div class="example-2">
+      <div id="one" class="split split-horizontal">
+        <p>#one</p>
+
+      </div>
+      <div id="two" class="split split-horizontal">
+        <div id="three" class="split split-vertical">
+          <p>#three</p>
+          <brace style="height: 500px"
+                 :fontsize="'12pt'"
+                 :theme="'github'"
+                 :mode="java"
+                 :codefolding="'markbegin'"
+                 :softwrap="'free'"
+                 :selectionstyle="'text'"
+                 :highlightline="true"
+          >
+          </brace>
+        </div>
+        <div id="four" class="split split-vertical">
+          <p>#four</p>
+        </div>
+      </div>
+    </div>
+
+  </v-card-flat>
+
+
+
+  </div>
 </template>
 
 <script>
   import axios from "axios";
-  import PROPERTIES from '../properties'
+  import PROPERTIES from '../properties';
   import marked from 'marked';
+  import Split from 'split.js'
+  import Brace from 'xen-brace'
+
+
   export default {
     name: 'lesson-card',
     data(){
@@ -26,8 +61,12 @@
           return marked(this.lesson.description, {sanitize: true})
       }
     },
+    components: {
+      Brace
+    },
     mounted(){
       this.fetchLesson();
+      this.split();
     },
     methods: {
       fetchLesson() {
@@ -46,7 +85,105 @@
           .then((response) => {
             this.lesson = response.data;
           });
+      },
+
+      split() {
+        Split(['#three', '#four'], {
+          direction: 'vertical'
+        });
+        Split(['#one', '#two'], {
+        });
       }
-    }
+    },
   }
 </script>
+
+<style>
+  .big-div {
+    min-height: 80vh !important;
+  }
+   td {
+     padding: 8px 10px;
+   }
+
+  td:first-child {
+    font-family: monospace;
+  }
+
+  h3 {
+    margin-top: 100px;
+  }
+
+  h5 {
+    margin-bottom: 0;
+  }
+
+  hr {
+    margin-top: 2px;
+  }
+
+
+  .example-2, .example-3, .example-4, .example-5, .example-6 {
+    height: 600px;
+    border: 1px solid #ddd;
+    border-radius: 4px;
+  }
+
+  .example-flex {
+    height: 200px;
+    border: 1px solid #ddd;
+    border-radius: 4px;
+    display: flex;
+    flex-direction: row;
+  }
+
+  .example-flex-reverse {
+    flex-direction: row-reverse;
+  }
+
+  #one, #two {
+    padding: 0px;
+  }
+
+  #one p {
+    padding: 0;
+  }
+
+  .example-4, .example-5 {
+    height: 400px;
+  }
+
+  .split p, .split-flex p {
+    padding: 20px;
+  }
+
+  .split, .split-flex {
+    -webkit-box-sizing: border-box;
+    -moz-box-sizing: border-box;
+    box-sizing: border-box;
+
+    overflow-y: auto;
+    overflow-x: hidden;
+  }
+
+  .gutter {
+    background-color: #eee;
+
+    background-repeat: no-repeat;
+    background-position: 50%;
+  }
+
+  .gutter.gutter-horizontal {
+    cursor: ew-resize;
+  }
+
+  .gutter.gutter-vertical {
+    cursor: ns-resize;
+  }
+
+  .split.split-horizontal, .gutter.gutter-horizontal {
+    height: 100%;
+    float: left;
+  }
+</style>
+
