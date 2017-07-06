eba3877442d9de2d51ec68e6241e41482623673c
Администратор <makspman@gmail.com>
2017-07-07 00:19:37 +0300

    lesson

--- front/src/components/LessonCard.vue
+++ front/src/components/LessonCard.vue
@@ -8,7 +8,7 @@
           <v-card-title >{{lesson.name}}</v-card-title>
         </div>
         <v-card-text class="white">
-          <div v-html="compiledMarkdown"></div>
+          <div v-html="compiledMarkdownDescr"></div>
         </v-card-text>
       </v-expansion-panel-content>
     </v-expansion-panel>
@@ -16,24 +16,40 @@
     <div class="example-2">
       <div id="one" class="split split-horizontal">
 
+        <div v-html="compiledMarkdownTheory"></div>
 
       </div>
       <div id="two" class="split split-horizontal">
         <div id="three" class="split split-vertical">
-
+          <v-app-bar>
+            <v-btn-dropdown v-bind:options="dropdown_font" v-model="fontSize"
+                            max-height="auto" editable="editable" label="Font size" overflow></v-btn-dropdown>
+            <v-btn-dropdown v-bind:options="dropdown_lang" v-model="lang"
+                            max-height="auto" editable="editable" label="Programming lang" overflow></v-btn-dropdown>
+            <v-btn flat primary dark default @click.native="runCode()">Run code</v-btn>
+          </v-app-bar>
           <brace style="height: 500px"
-                 :fontsize="'12pt'"
+                 :fontsize="fontSize.text"
                  :theme="'github'"
-                 :mode="java"
+                 :mode="lang.text"
                  :codefolding="'markbegin'"
                  :softwrap="'free'"
                  :selectionstyle="'text'"
                  :highlightline="true"
+                 :dataedit="content"
+                 @change="getCode($event)"
           >
           </brace>
+
         </div>
         <div id="four" class="split split-vertical">
-
+          <v-text-field
+            name="input-7-1"
+            label="Result"
+            :value="formattedResponse"
+            multi-line
+            disabled>
+          </v-text-field>
         </div>
       </div>
     </div>
@@ -53,17 +69,45 @@
 
 
   export default {
+
     name: 'lesson-card',
     data(){
       return {
+        response: '',
         lesson: Object,
-        expanded: 'True'
+        content: 'public class HelloWorld {\n public static void main(String[] args) {\n    System.out.println("Hello, World");\n }\n}',
+        expanded: 'True',
+        dropdown_font: [
+          { text: '12pt' },
+          { text: '14pt' },
+          { text: '16pt' },
+          { text: '18pt' },
+          { text: '20pt' }
+        ],
+        dropdown_lang: [
+          { text: 'java' },
+          { text: 'html' },
+          { text: 'javascript' },
+          { text: 'json' }
+        ],
+        fontSize: {text: '12pt'},
+        lang: { text : 'java'}
       }
     },
     computed: {
-      compiledMarkdown: function () {
+      compiledMarkdownDescr: function () {
         if (this.lesson.description != undefined)
           return marked(this.lesson.description, {sanitize: true})
+      },
+      compiledMarkdownTheory: function () {
+        if (this.lesson.theory != undefined)
+          return marked(this.lesson.theory, {sanitize: true})
+      },
+      formattedResponse() {
+        return this.response ?
+          this.response.methodResult ?
+            `${this.response.methodResult.systemOut}`
+            : `Error: ${this.response.generalResponse.message}` : ''
       }
     },
     components: {
@@ -104,6 +148,16 @@
           minSize: [0,0],
           gutterSize: 5
         });
+      },
+      getCode(target){
+        this.content = target;
+      },
+      runCode() {
+        const code = {sourceCode: this.content};
+        axios.post(PROPERTIES.HOST + '/run-class', code)
+          .then((response) => {
+            this.response = response.data;
+          });
       }
     },
   }
